e6ab2708dc5908c979e552e30db722bc39b1c0c8
PremiRam <79751930+PremiRam@users.noreply.github.com>
2021-06-10 18:02:18 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connecton')
-    res.end('Hello- now with continuos deployment- its so awesome')
+    res.end('Hello everyone- now with continuos deployment- its so awesome')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
